9d244cd6f7723342f1f4d9f15e7efe065dc61af2
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-11-07 18:10:57 +0500

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,15 +28,15 @@ function Home() {
                     aria-label="Toggle menu"
                 >
                     {isMenuOpen ? <ImCross /> : <GiHamburgerMenu />}
-                </button>           
+                </button>
             </nav>
-            <main className="flex xs:px-4 xs:flex-col-reverse mt-12 items-center lg:flex-row lg:justify-around ">
+            <main className="flex xs:px-4 md:px-20 xs:flex-col-reverse mt-16 lg:flex-row lg:justify-around">
                 <div className="relative">
                     <p className="text-[#EF6D58] mb-4 tracking-widest">
                         MODERN STUDIO
                     </p>
                     <div>
-                    <h1 className="xs:text-5xl md:text-6xl md:w-[8em] font-bold z-20 relative">
+                        <h1 className="xs:text-5xl md:text-6xl md:w-[8em] font-bold z-20 relative">
                             We're Help To Build Your Dream Project
                         </h1>
                         <img
@@ -45,8 +45,20 @@ function Home() {
                             className="z-0 absolute top-0 right-5"
                         />
                     </div>
+                    <p className="text-[#B2B2BA] mt-8 md:w-[26em] font-epilogue">
+                        Agency provides a full serive range including technical
+                        skills, design, business understanding.
+                    </p>
+                    <div className="flex mt-8 font-epilogue gap-8">
+                        <button className="bg-[#EF6D58] rounded-lg px-4 py-3 font-bold">
+                            HOW WE WORK
+                        </button>
+                        <p className="bg-none px-4 py-3">Contact Us</p>
+                    </div>
                 </div>
-                <img src="./hero.png" className="w-min" alt=""/>
+                <div className="flex items-center justify-center">
+                    <img src="./hero.png" className="w-min" alt="" />
+                </div>
             </main>
         </div>
     );
